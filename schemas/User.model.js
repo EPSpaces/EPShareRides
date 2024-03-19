192181796ea2ae271c89a5b9fa7b85f20da06b6f
asimplecomplex <30184083-asimplecomplex@users.noreply.replit.com>
2024-03-19 03:42:49 +0000

    More changes

---
+++ schemas/User.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  admin: { type: Boolean, default: false }
+});
+
+module.exports = mongoose.model('User', userSchema);
