abbdf576be13c6f966be9ca595983718f9709678
ajosan123 <ajosan@eastsideprep.org>
2025-01-02 15:24:36 -0800

    user settings comments

--- schemas/UserSettings.model.js
+++ schemas/UserSettings.model.js
@@ -1,8 +1,11 @@
+// Purpose: Define the schema for the UserSettings collection in the database.
 const mongoose = require('mongoose');
 
+// Define the schema for the UserSettings collection in the database.
 const userSettingsSchema = new mongoose.Schema({
   userEmail: { type: String, required: true },
   showAccToOthers: { type: Boolean, default: true }
 });
 
+// Export the UserSettings model.
 module.exports = mongoose.model('UserSettings', userSettingsSchema);
