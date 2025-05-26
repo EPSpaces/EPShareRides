3caec09fcd8246c7b421a6d18233428717f79d61
Nikhil Mahesh <60377182+N-Mahesh@users.noreply.github.com>
2025-05-26 13:55:51 -0700

    Change all names from EPShare Rides to EPCarpool
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/internot169/EPShareRides?shareId=XXXX-XXXX-XXXX-XXXX).

--- public/config.js
+++ public/config.js
@@ -1,8 +1,8 @@
 const config = {
     apiKey: "AIzaSyDRbxTFNtN4rwZymQkruxAO-ddofaCKzIM",
-    authDomain: "epshare-rides.firebaseapp.com",
-    projectId: "epshare-rides",
-    storageBucket: "epshare-rides.firebasestorage.app",
+    authDomain: "epcarpool.firebaseapp.com",
+    projectId: "epcarpool",
+    storageBucket: "epcarpool.firebasestorage.app",
     messagingSenderId: "1086500164316",
     appId: "1:1086500164316:web:d054b1d554ee0314931a19",
     measurementId: "G-3DCP1Y6GXH"
